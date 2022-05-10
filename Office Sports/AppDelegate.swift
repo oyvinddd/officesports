fbcdbfef72a45aa032399f660cc74d0997d8a581
Øyvind Hauge <oyvind.s.hauge@gmail.com>
2022-05-10 17:15:20 +0200

    initial commit

---
+++ Office Sports/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Office Sports
+//
+//  Created by Øyvind Hauge on 10/05/2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = MainViewController()
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+}
+
---
+++ Office Sports/Screens/MainViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MainViewController.swift
+//  Office Sports
+//
+//  Created by Øyvind Hauge on 10/05/2022.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
