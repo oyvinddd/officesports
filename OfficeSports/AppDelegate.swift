4b0933b0f984b18f15a876b54c31481b4da4ae8d
Øyvind Hauge <oyvind.s.hauge@gmail.com>
2022-05-12 16:42:18 +0200

    adding firebase auth

--- OfficeSports/AppDelegate.swift
+++ OfficeSports/AppDelegate.swift
@@ -6,12 +6,14 @@
 //
 
 import UIKit
+import FirebaseCore
 
 @main class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
         initRootViewController()
         return true
     }
