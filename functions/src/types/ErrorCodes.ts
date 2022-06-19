7f4b64522e0160704945393d840ea8cbc7fa87f0
Sindre Bøyum <sindre.boyum@tietoevry.com>
2022-06-19 17:46:50 +0200

    add error codes, improve player validation
    Removes possibility for matches where winnerId and playerId are equal.
    
    Adds error code enum that can be checked against in the client app

---
+++ functions/src/types/ErrorCodes.ts
@@ -0,0 +1,9 @@
+export enum ErrorCodes {
+  InvalidWinnerId = "ERROR0001",
+  InvalidLoserId = "ERROR0002",
+  InvalidPlayerIds = "ERROR0003",
+  InvalidSport = "ERROR0004",
+  WinMatchMethodNotAllowed = "ERROR0005",
+  WinnerNotFound = "ERROR0006",
+  LoserNotFound = "ERROR0007",
+}
