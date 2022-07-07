3d4f1b6a940ca5225fb766c6bc05a22d0501b665
Sindre Bøyum <sindre.boyum@tietoevry.com>
2022-07-07 13:57:29 +0200

    endpoint for authenticating Slack app
    This is needed as an authentication step when installing a new app to a
    Slack workspace. The endpoint's URL is set as the Slack app's redirect
    URI

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -22,116 +22,152 @@ import { Season } from "./types/Season";
 import { Sport } from "./types/Sport";
 import { WinMatchBody } from "./types/WinMatchBody";
 
-export const winMatch = functions.https.onRequest(async (request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
+export const winMatch = functions.https.onRequest(
+  async (request, response): Promise<void> => {
+    functions.logger.info("Hello logs!", { structuredData: true });
 
-  const isPost = request.method === "POST";
-  if (!isPost) {
-    sendErrorStatus(response, HttpStatus.METHOD_NOT_ALLOWED, [
-      {
-        errorCode: ErrorCodes.WinMatchMethodNotAllowed,
-        message: `Method '${request.method}' is not allowed`,
-      },
-    ]);
-    return;
-  }
-
-  const bodyErrors = validateWinMatchBody(request.body);
-  if (bodyErrors) {
-    sendErrorStatus(response, HttpStatus.BAD_REQUEST, bodyErrors);
-  }
-
-  const { winnerId, loserId, sport }: WinMatchBody = request.body;
-
-  console.log({ body: request.body });
-
-  const winner = await getPlayer(winnerId);
-  const loser = await getPlayer(loserId);
-
-  console.log({ winnerId, loserId, sport, winner, loser });
-
-  if (!winner) {
-    sendErrorStatus(response, HttpStatus.BAD_REQUEST, [
-      {
-        errorCode: ErrorCodes.WinnerNotFound,
-        message: `Player with id '${winnerId} not found`,
-      },
-    ]);
-    return;
-  }
-
-  if (!loser) {
-    sendErrorStatus(response, HttpStatus.BAD_REQUEST, [
-      {
-        errorCode: ErrorCodes.LoserNotFound,
-        message: `Player with id '${loserId} not found`,
-      },
-    ]);
-    return;
-  }
-
-  setEmptyPlayerStats(winner, initialScore);
-  setEmptyPlayerStats(loser, initialScore);
-
-  const isFoosball = sport === Sport.Foosball;
-  const isTableTennis = sport === Sport.TableTennis;
-
-  const oldWinnerScore =
-    (isFoosball
-      ? winner.foosballStats?.score
-      : winner.tableTennisStats?.score) ?? initialScore;
-
-  const oldLoserScore =
-    (isFoosball ? loser.foosballStats?.score : loser.tableTennisStats?.score) ??
-    initialScore;
-
-  const { playerRating: newWinnerScore, opponentRating: newLoserScore } =
-    EloRating.calculate(oldWinnerScore, oldLoserScore);
-
-  console.log({ newWinnerScore, newLoserScore });
-
-  const match: Match = {
-    date: new firebase.firestore.Timestamp(Math.floor(Date.now() / 1000), 0),
-    sport,
-    winner,
-    loser,
-    winnerDelta: newWinnerScore - oldWinnerScore,
-    loserDelta: newLoserScore - oldLoserScore,
-  };
-
-  addMatch(match);
-
-  if (isFoosball) {
-    if (!winner.foosballStats || !loser.foosballStats) {
-      throw new Error("Missing foosball stats");
+    const isPost = request.method === "POST";
+    if (!isPost) {
+      sendErrorStatus(response, HttpStatus.METHOD_NOT_ALLOWED, [
+        {
+          errorCode: ErrorCodes.WinMatchMethodNotAllowed,
+          message: `Method '${request.method}' is not allowed`,
+        },
+      ]);
+      return;
     }
 
-    winner.foosballStats.matchesPlayed += 1;
-    winner.foosballStats.score = newWinnerScore;
-
-    loser.foosballStats.matchesPlayed += 1;
-    loser.foosballStats.score = newLoserScore;
-  }
-
-  if (isTableTennis) {
-    if (!winner.tableTennisStats || !loser.tableTennisStats) {
-      throw new Error("Missing foosball stats");
+    const bodyErrors = validateWinMatchBody(request.body);
+    if (bodyErrors) {
+      sendErrorStatus(response, HttpStatus.BAD_REQUEST, bodyErrors);
     }
 
-    winner.tableTennisStats.matchesPlayed += 1;
-    winner.tableTennisStats.score = newWinnerScore;
+    const { winnerId, loserId, sport }: WinMatchBody = request.body;
 
-    loser.tableTennisStats.matchesPlayed += 1;
-    loser.tableTennisStats.score = newLoserScore;
-  }
+    console.log({ body: request.body });
 
-  updatePlayer(winner);
-  updatePlayer(loser);
+    const winner = await getPlayer(winnerId);
+    const loser = await getPlayer(loserId);
 
-  console.log({ match });
+    console.log({ winnerId, loserId, sport, winner, loser });
 
-  response.send(match);
-});
+    if (!winner) {
+      sendErrorStatus(response, HttpStatus.BAD_REQUEST, [
+        {
+          errorCode: ErrorCodes.WinnerNotFound,
+          message: `Player with id '${winnerId} not found`,
+        },
+      ]);
+      return;
+    }
+
+    if (!loser) {
+      sendErrorStatus(response, HttpStatus.BAD_REQUEST, [
+        {
+          errorCode: ErrorCodes.LoserNotFound,
+          message: `Player with id '${loserId} not found`,
+        },
+      ]);
+      return;
+    }
+
+    setEmptyPlayerStats(winner, initialScore);
+    setEmptyPlayerStats(loser, initialScore);
+
+    const isFoosball = sport === Sport.Foosball;
+    const isTableTennis = sport === Sport.TableTennis;
+
+    const oldWinnerScore =
+      (isFoosball
+        ? winner.foosballStats?.score
+        : winner.tableTennisStats?.score) ?? initialScore;
+
+    const oldLoserScore =
+      (isFoosball
+        ? loser.foosballStats?.score
+        : loser.tableTennisStats?.score) ?? initialScore;
+
+    const { playerRating: newWinnerScore, opponentRating: newLoserScore } =
+      EloRating.calculate(oldWinnerScore, oldLoserScore);
+
+    console.log({ newWinnerScore, newLoserScore });
+
+    const match: Match = {
+      date: new firebase.firestore.Timestamp(Math.floor(Date.now() / 1000), 0),
+      sport,
+      winner,
+      loser,
+      winnerDelta: newWinnerScore - oldWinnerScore,
+      loserDelta: newLoserScore - oldLoserScore,
+    };
+
+    addMatch(match);
+
+    if (isFoosball) {
+      if (!winner.foosballStats || !loser.foosballStats) {
+        throw new Error("Missing foosball stats");
+      }
+
+      winner.foosballStats.matchesPlayed += 1;
+      winner.foosballStats.score = newWinnerScore;
+
+      loser.foosballStats.matchesPlayed += 1;
+      loser.foosballStats.score = newLoserScore;
+    }
+
+    if (isTableTennis) {
+      if (!winner.tableTennisStats || !loser.tableTennisStats) {
+        throw new Error("Missing foosball stats");
+      }
+
+      winner.tableTennisStats.matchesPlayed += 1;
+      winner.tableTennisStats.score = newWinnerScore;
+
+      loser.tableTennisStats.matchesPlayed += 1;
+      loser.tableTennisStats.score = newLoserScore;
+    }
+
+    updatePlayer(winner);
+    updatePlayer(loser);
+
+    console.log({ match });
+
+    response.send(match);
+  },
+);
+
+export const connectSlackApp = functions
+  .runWith({
+    secrets: ["SLACK_CLIENT_ID", "SLACK_CLIENT_SECRET", "SLACK_TOKEN"],
+  })
+  .https.onRequest(async (request, response): Promise<void> => {
+    const { originalUrl, path, params, body } = request;
+    console.log({ originalUrl, path, params, body });
+
+    let { code } = request.query;
+
+    if (Array.isArray(code)) {
+      code = code[0];
+    }
+
+    if (!code) {
+      console.error("Missing 'code' query param");
+      response.send("Missing 'code' query param");
+
+      return;
+    }
+
+    const accessToken = await slackHelpers.authenticate(code.toString());
+
+    response.send({
+      originalUrl,
+      path,
+      params,
+      body,
+      accessToken,
+      isSuccess: !!accessToken,
+    });
+  });
 
 const resetScoreboardsFunction = async () => {
   const seasonWinnerFoosball = await getLeader(Sport.Foosball);
