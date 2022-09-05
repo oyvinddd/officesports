0a1439c893829e71836b42c8597dc6664a3c5306
Øyvind Hauge <oyvind.s.hauge@gmail.com>
2022-09-05 15:13:04 +0200

    drop shadow on emoji

---
+++ OfficeSports/Extensions/UILabel+Visuals.swift
@@ -0,0 +1,20 @@
+//
+//  UILabel+Visuals.swift
+//  OfficeSports
+//
+//  Created by Øyvind Hauge on 05/09/2022.
+//
+
+import Foundation
+import UIKit
+
+extension UILabel {
+    
+    func applyLargeDropShadow(_ backgroundColor: UIColor) {
+        layer.shadowColor = backgroundColor.cgColor
+        layer.shadowRadius = 20
+        layer.shadowOpacity = 0.8
+        layer.shadowOffset = .zero
+        layer.masksToBounds = false
+    }
+}
