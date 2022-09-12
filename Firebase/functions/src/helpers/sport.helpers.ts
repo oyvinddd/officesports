329356ef66d89d431327de6d585e7079d83c4dcc
Sindre Bøyum <sindre.boyum@tietoevry.com>
2022-09-12 14:13:00 +0200

    fix: use `foosballStats` etc instead of `stats`

--- Firebase/functions/src/helpers/sport.helpers.ts
+++ Firebase/functions/src/helpers/sport.helpers.ts
@@ -28,10 +28,19 @@ export const getSportStats = (player: Player, sport: Sport): Stats => {
     ];
   }
 
-  const sportStats = player.stats.find(stat => stat.sport === sport);
-  if (sportStats) {
-    return sportStats;
+  switch (sport) {
+    case Sport.Foosball:
+      return (player.foosballStats ??= getEmptyStats(Sport.Foosball));
+    case Sport.TableTennis:
+      return (player.tableTennisStats ??= getEmptyStats(Sport.TableTennis));
+    case Sport.Pool:
+      return (player.poolStats ??= getEmptyStats(Sport.Pool));
   }
+
+  // const sportStats = player.stats.find(stat => stat.sport === sport);
+  // if (sportStats) {
+  //   return sportStats;
+  // }
 
   return getEmptyStats(sport);
 };
