5bbb54b6043f9c38d69120d9bd7d3d1455cb5815
Øyvind Hauge <oyvind.s.hauge@gmail.com>
2022-09-16 09:38:13 +0200

    sign in with apple firebase implementation WIP

--- OfficeSports/API/FirebaseSportsAPI.swift
+++ OfficeSports/API/FirebaseSportsAPI.swift
@@ -5,6 +5,8 @@
 //  Created by Øyvind Hauge on 19/05/2022.
 //
 
+import CryptoKit
+import AuthenticationServices
 import FirebaseCore
 import GoogleSignIn
 import FirebaseAuth
@@ -50,6 +52,8 @@ final class FirebaseSportsAPI: SportsAPI {
         database.collection(fbTeamsCollection)
     }
     
+    private var currentNonce: String?
+    
     func signInWithGoogle(from viewController: UIViewController, result: @escaping (Result<Bool, Error>) -> Void) {
         guard let clientID = FirebaseApp.app()?.options.clientID else {
             return
@@ -81,7 +85,19 @@ final class FirebaseSportsAPI: SportsAPI {
         }
     }
     
+    // https://firebase.google.com/docs/auth/ios/apple?authuser=0&hl=en
     func signInWithApple(from viewController: UIViewController, result: @escaping ((Result<Bool, Error>) -> Void)) {
+        let nonce = randomNonceString()
+        currentNonce = nonce
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        request.nonce = sha256(nonce)
+        
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
     }
     
     func signOut() -> Error? {
@@ -282,7 +298,7 @@ final class FirebaseSportsAPI: SportsAPI {
         // find invites the current user has sent that are not older than 24 hours
         let query = invitesCollection
             .whereField("inviterId", isEqualTo: uid)
-            // .whereField("date", isGreaterThan: date)
+        // .whereField("date", isGreaterThan: date)
         
         query.getDocuments { (snapshot, error) in
             if let error = error {
@@ -395,5 +411,91 @@ final class FirebaseSportsAPI: SportsAPI {
         case .pool:
             return "poolStats.score"
         }
+    }
+}
+
+extension FirebaseSportsAPI: ASAuthorizationControllerDelegate {
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            guard let nonce = currentNonce else {
+                fatalError("Invalid state: A login callback was received, but no login request was sent.")
+            }
+            guard let appleIDToken = appleIDCredential.identityToken else {
+                print("Unable to fetch identity token")
+                return
+            }
+            guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+                print("Unable to serialize token string from data: \(appleIDToken.debugDescription)")
+                return
+            }
+            // Initialize a Firebase credential.
+            let credential = OAuthProvider.credential(withProviderID: "apple.com",
+                                                      idToken: idTokenString,
+                                                      rawNonce: nonce)
+            // Sign in with Firebase.
+            Auth.auth().signIn(with: credential) { (authResult, error) in
+                guard let error = error else {
+                    // Error. If error.code == .MissingOrInvalidNonce, make sure
+                    // you're sending the SHA256-hashed nonce as a hex string with
+                    // your request to Apple.
+                    print(error)
+                    return
+                }
+                // User is signed in to Firebase with Apple.
+                // ...
+            }
+        }
+    }
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        // Handle error.
+        print("Sign in with Apple errored: \(error)")
+    }
+    
+    // Random 'nonce' string used when signing in with Apple
+    // Adapted from https://auth0.com/docs/api-auth/tutorials/nonce#generate-a-cryptographically-random-nonce
+    private func randomNonceString(length: Int = 32) -> String {
+        precondition(length > 0)
+        let charset: [Character] =
+        Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+        var result = ""
+        var remainingLength = length
+        
+        while remainingLength > 0 {
+            let randoms: [UInt8] = (0 ..< 16).map { _ in
+                var random: UInt8 = 0
+                let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+                if errorCode != errSecSuccess {
+                    fatalError(
+                        "Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)"
+                    )
+                }
+                return random
+            }
+            
+            randoms.forEach { random in
+                if remainingLength == 0 {
+                    return
+                }
+                
+                if random < charset.count {
+                    result.append(charset[Int(random)])
+                    remainingLength -= 1
+                }
+            }
+        }
+        
+        return result
+    }
+    
+    private func sha256(_ input: String) -> String {
+        let inputData = Data(input.utf8)
+        let hashedData = SHA256.hash(data: inputData)
+        let hashString = hashedData.compactMap {
+            String(format: "%02x", $0)
+        }.joined()
+        
+        return hashString
     }
 }
