b0ae723b6f19d6e1c163ff99185783a6dfe5c661
Sindre Bøyum <boyum@users.noreply.github.com>
2022-09-23 08:28:03 +0200

    fix: publish upsert player (#95)

--- Firebase/functions/src/index.ts
+++ Firebase/functions/src/index.ts
@@ -2,3 +2,4 @@ export { connectSlackApp } from "./api/connectSlackApp";
 export { resetScoreboardsCron } from "./api/resetScoreboardsCron";
 export { slackGetLeader } from "./api/slackGetLeader";
 export { winMatch } from "./api/winMatch";
+export { upsertPlayer } from "./api/upsertPlayer";
