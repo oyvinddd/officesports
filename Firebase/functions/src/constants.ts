6af0fc3e2b923b6c5199189dd4e3a1dbfac62168
Sindre Bøyum <boyum@users.noreply.github.com>
2022-09-30 19:58:57 +0200

    feat: add Banking to Slack season summary message (#105)

--- Firebase/functions/src/constants.ts
+++ Firebase/functions/src/constants.ts
@@ -1,4 +1,5 @@
 export const initialScore = 1200;
 export const tietoevryCreateTeamId = "WTioAXuBpsAp0nLimN4p";
+export const tietoevryBankingTeamId = "QUtC149dOnloqxKwAxpz";
 
 export const testIds = ["test_1"];
