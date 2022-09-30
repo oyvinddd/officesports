cad1a63666052d41525a26c1e116cf0548762872
Øyvind Hauge <oyvind.s.hauge@gmail.com>
2022-09-30 16:10:58 +0200

    locking down teams WIP

---
+++ OfficeSports/Screens/Onboarding/ViewControllers/JoinTeamViewController.swift
@@ -0,0 +1,43 @@
+//
+//  JoinTeamViewController.swift
+//  OfficeSports
+//
+//  Created by Øyvind Hauge on 29/09/2022.
+//
+
+import UIKit
+
+final class JoinTeamViewController: UIViewController {
+
+    private let viewModel: JoinTeamViewModel
+    
+    init(viewModel: JoinTeamViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        applySheetPresentation()
+        setupChildViews()
+        configureUI()
+    }
+    
+    private func setupChildViews() {
+        
+    }
+    
+    private func configureUI() {
+        view.backgroundColor = .white
+    }
+}
+
+// MARK: - Input Field
+
+private final class InputField: UIView {
+    
+}
--- OfficeSports/Screens/Scanner/Views/DuoProgressView.swift
+++ OfficeSports/Screens/Scanner/Views/DuoProgressView.swift
@@ -7,75 +7,49 @@
 
 import UIKit
 
-private let playerImageDiameter: CGFloat = 50
-private let playerImageRadius = playerImageDiameter / 2
+protocol DuoProgressViewDelegate: AnyObject {
+    
+    func didRegisterPlayer(_ player: OSPlayer)
+    
+    func didFinishRegisteringDuoMatch(_ registration: OSMatchRegistration)
+}
 
 final class DuoProgressView: UIView {
     
     private lazy var stackView: UIStackView = {
-        return UIStackView.createStackView(.clear, axis: .horizontal)
+        return UIStackView.createStackView(.clear, axis: .horizontal, spacing: 4)
     }()
     
-    private lazy var player1ImageWrap: UIView = {
-        let imageWrap = UIView.createView(.white, cornerRadius: playerImageRadius)
-        imageWrap.applyMediumDropShadow(.black)
-        return imageWrap
+    private lazy var player1View: PlayerView = {
+        return PlayerView()
     }()
     
-    private lazy var player1ImageBackground: UIView = {
-        let profileImageBackground = UIView.createView(.blue)
-        profileImageBackground.applyCornerRadius((playerImageDiameter - 16) / 2)
-        return profileImageBackground
+    private lazy var player2View: PlayerView = {
+        return PlayerView()
     }()
     
-    private lazy var player2ImageWrap: UIView = {
-        let imageWrap = UIView.createView(.white, cornerRadius: playerImageRadius)
-        imageWrap.applyMediumDropShadow(.black)
-        return imageWrap
+    private lazy var player3View: PlayerView = {
+        return PlayerView()
     }()
     
-    private lazy var player2ImageBackground: UIView = {
-        let profileImageBackground = UIView.createView(.blue)
-        profileImageBackground.applyCornerRadius((playerImageDiameter - 16) / 2)
-        return profileImageBackground
+    private lazy var player4View: PlayerView = {
+        return PlayerView()
     }()
     
-    private lazy var player3ImageWrap: UIView = {
-        let imageWrap = UIView.createView(.white, cornerRadius: playerImageRadius)
-        imageWrap.applyMediumDropShadow(.black)
-        return imageWrap
-    }()
+    private var winner1: OSPlayer?
+    private var winner2: OSPlayer?
+    private var loser1: OSPlayer?
+    private var loser2: OSPlayer?
     
-    private lazy var player3ImageBackground: UIView = {
-        let profileImageBackground = UIView.createView(.blue)
-        profileImageBackground.applyCornerRadius((playerImageDiameter - 16) / 2)
-        return profileImageBackground
-    }()
+    weak var delegate: DuoProgressViewDelegate?
     
-    private lazy var player4ImageWrap: UIView = {
-        let imageWrap = UIView.createView(.white, cornerRadius: playerImageRadius)
-        imageWrap.applyMediumDropShadow(.black)
-        return imageWrap
-    }()
-    
-    private lazy var player4ImageBackground: UIView = {
-        let profileImageBackground = UIView.createView(.blue)
-        profileImageBackground.applyCornerRadius((playerImageDiameter - 16) / 2)
-        return profileImageBackground
-    }()
-    
-    private lazy var cancelButton: OSButton = {
-        let button = OSButton("Cancel", type: .secondaryInverted)
-        button.addTarget(self, action: #selector(cancelButtonTapped), for: .touchUpInside)
-        return button
-    }()
-    
-    init() {
+    init(delegate: DuoProgressViewDelegate?) {
         super.init(frame: .zero)
         translatesAutoresizingMaskIntoConstraints = false
         backgroundColor = .white
         setupChildViews()
         applyCornerRadius(8)
+        self.delegate = delegate
     }
     
     required init?(coder: NSCoder) {
@@ -83,25 +57,81 @@ final class DuoProgressView: UIView {
     }
     
     private func setupChildViews() {
-        addSubview(stackView)
-        addSubview(cancelButton)
-        
-        stackView.addArrangedSubviews(player1ImageWrap, player2ImageWrap, player3ImageWrap, player4ImageWrap)
-        
-        NSLayoutConstraint.activate([
-            stackView.leftAnchor.constraint(equalTo: leftAnchor),
-            stackView.rightAnchor.constraint(equalTo: rightAnchor),
-            stackView.topAnchor.constraint(equalTo: topAnchor),
-            stackView.bottomAnchor.constraint(equalTo: cancelButton.topAnchor, constant: -16),
-            cancelButton.leftAnchor.constraint(equalTo: leftAnchor, constant: 8),
-            cancelButton.rightAnchor.constraint(equalTo: rightAnchor, constant: -8),
-            cancelButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -8),
-            player1ImageWrap.widthAnchor.constraint(equalToConstant: playerImageDiameter),
-            player1ImageWrap.heightAnchor.constraint(equalTo: player1ImageWrap.widthAnchor)
-        ])
+        NSLayoutConstraint.pinToView(self, stackView, padding: 4)
+        stackView.addArrangedSubviews(player1View, player2View, player3View, player4View)
     }
     
-    @objc private func cancelButtonTapped(_ sender: UIButton) {
+    func toggle(show: Bool, animated: Bool) {
+        let interval = TimeInterval(integerLiteral: 1)
         
+        UIView.animate(
+            withDuration: interval,
+            delay: 0,
+            usingSpringWithDamping: 0.8,
+            initialSpringVelocity: 0.5) {
+                
+            }
+    }
+    
+    func updateWithPlayer(_ player: OSPlayer) {
+        if winner1 == nil {
+            winner1 = player
+        } else if winner2 == nil {
+            winner2 = player
+        } else if loser1 == nil {
+            loser1 = player
+        } else {
+            loser2 = player
+        }
+    }
+    
+    func reset() {
+        winner1 = OSAccount.current.player
+        winner2 = nil
+        loser1 = nil
+        loser2 = nil
+    }
+}
+
+// MARK: - Player View
+
+private let playerImageDiameter: CGFloat = 50
+private let playerImageRadius = playerImageDiameter / 2
+
+private final class PlayerView: UIView {
+    
+    private lazy var playerEmojiBackground: UIView = {
+        let profileImageBackground = UIView.createView(.blue)
+        profileImageBackground.applyCornerRadius((playerImageRadius - 16) / 2)
+        return profileImageBackground
+    }()
+    
+    private lazy var playerEmojiLabel: UILabel = {
+        let label = UILabel.createLabel(.black, alignment: .center)
+        label.font = UIFont.systemFont(ofSize: 50)
+        return label
+    }()
+    
+    private lazy var emptyPlayerImageView: UIImageView = {
+        let image = UIImage(systemName: "person.crop.circle.badge.questionmark")!.withRenderingMode(.alwaysTemplate)
+        let imageView = UIImageView.createImageView(image)
+        imageView.tintColor = UIColor.OS.Text.disabled
+        return imageView
+    }()
+    
+    init() {
+        super.init(frame: .zero)
+        translatesAutoresizingMaskIntoConstraints = false
+        setupChildViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupChildViews() {
+        NSLayoutConstraint.pinToView(self, playerEmojiBackground)
+        NSLayoutConstraint.pinToView(playerEmojiBackground, playerEmojiLabel)
+        NSLayoutConstraint.pinToView(playerEmojiBackground, emptyPlayerImageView)
     }
 }
