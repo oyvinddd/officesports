64f15e66166f51f054462a382f0edfc174621445
Øyvind Hauge <oyvind.s.hauge@gmail.com>
2023-01-02 11:44:11 +0100

    minor auth refactoring

--- OfficeSports/API/FirebaseSportsAPI.swift
+++ OfficeSports/API/FirebaseSportsAPI.swift
@@ -29,7 +29,7 @@ private let maxResultsInScoreboard = 200
 private let maxResultsInRecentMatches = 300
 private let maxResultsInLatestMatches = 10
 
-// swiftlint:disable file_length type_body_length force_cast
+// swiftlint:disable file_length type_body_length
 final class FirebaseSportsAPI: NSObject, SportsAPI {
     
     private let database = Firestore.firestore()
@@ -88,16 +88,16 @@ final class FirebaseSportsAPI: NSObject, SportsAPI {
     }
     
     func signInWithApple(from viewController: UIViewController, result: @escaping ((Result<Bool, Error>) -> Void)) {
-        let nonce = randomNonceString()
+        let nonce = AuthUtils.randomNonceString()
         currentNonce = nonce
         let appleIDProvider = ASAuthorizationAppleIDProvider()
         let request = appleIDProvider.createRequest()
         request.requestedScopes = [.email]
-        request.nonce = sha256(nonce)
+        request.nonce = AuthUtils.sha256(nonce)
         
         let authorizationController = ASAuthorizationController(authorizationRequests: [request])
-        authorizationController.delegate = self
-        authorizationController.presentationContextProvider = viewController as! any ASAuthorizationControllerPresentationContextProviding
+        authorizationController.delegate = viewController as? any ASAuthorizationControllerDelegate
+        authorizationController.presentationContextProvider = viewController as? any ASAuthorizationControllerPresentationContextProviding
         authorizationController.performRequests()
     }
     
@@ -412,91 +412,5 @@ final class FirebaseSportsAPI: NSObject, SportsAPI {
         case .pool:
             return "poolStats.score"
         }
-    }
-}
-
-extension FirebaseSportsAPI: ASAuthorizationControllerDelegate {
-    
-    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
-        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
-            guard let nonce = currentNonce else {
-                fatalError("Invalid state: A login callback was received, but no login request was sent.")
-            }
-            guard let appleIDToken = appleIDCredential.identityToken else {
-                print("Unable to fetch identity token")
-                return
-            }
-            guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
-                print("Unable to serialize token string from data: \(appleIDToken.debugDescription)")
-                return
-            }
-            // Initialize a Firebase credential.
-            let credential = OAuthProvider.credential(withProviderID: "apple.com",
-                                                      idToken: idTokenString,
-                                                      rawNonce: nonce)
-            // Sign in with Firebase.
-            Auth.auth().signIn(with: credential) { (authResult, error) in
-                guard let error = error else {
-                    // Error. If error.code == .MissingOrInvalidNonce, make sure
-                    // you're sending the SHA256-hashed nonce as a hex string with
-                    // your request to Apple.
-                    print(error)
-                    return
-                }
-                // User is signed in to Firebase with Apple.
-                // ...
-            }
-        }
-    }
-    
-    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
-        // Handle error.
-        print("Sign in with Apple errored: \(error)")
-    }
-    
-    // Random 'nonce' string used when signing in with Apple
-    // Adapted from https://auth0.com/docs/api-auth/tutorials/nonce#generate-a-cryptographically-random-nonce
-    private func randomNonceString(length: Int = 32) -> String {
-        precondition(length > 0)
-        let charset: [Character] =
-        Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
-        var result = ""
-        var remainingLength = length
-        
-        while remainingLength > 0 {
-            let randoms: [UInt8] = (0 ..< 16).map { _ in
-                var random: UInt8 = 0
-                let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
-                if errorCode != errSecSuccess {
-                    fatalError(
-                        "Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)"
-                    )
-                }
-                return random
-            }
-            
-            randoms.forEach { random in
-                if remainingLength == 0 {
-                    return
-                }
-                
-                if random < charset.count {
-                    result.append(charset[Int(random)])
-                    remainingLength -= 1
-                }
-            }
-        }
-        
-        return result
-    }
-    
-    private func sha256(_ input: String) -> String {
-        let inputData = Data(input.utf8)
-        let hashedData = SHA256.hash(data: inputData)
-        let hashString = hashedData.compactMap {
-            String(format: "%02x", $0)
-        }.joined()
-        
-        return hashString
     }
 }
--- OfficeSports/Screens/Onboarding/ViewControllers/WelcomeViewController.swift
+++ OfficeSports/Screens/Onboarding/ViewControllers/WelcomeViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import Combine
 import AuthenticationServices
+import FirebaseAuth
 
 final class WelcomeViewController: UIViewController {
     
@@ -55,6 +56,7 @@ final class WelcomeViewController: UIViewController {
     
     private let viewModel: AuthViewModel
     private var subscribers = Set<AnyCancellable>()
+    private var currentNonce: String?
     
     init(viewModel: AuthViewModel) {
         self.viewModel = viewModel
@@ -135,6 +137,46 @@ final class WelcomeViewController: UIViewController {
     
     @objc private func appleButtonTapped(_ sender: UIButton) {
         viewModel.signInWithApple(from: self)
+    }
+}
+
+extension WelcomeViewController: ASAuthorizationControllerDelegate {
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            guard let nonce = currentNonce else {
+                fatalError("Invalid state: A login callback was received, but no login request was sent.")
+            }
+            guard let appleIDToken = appleIDCredential.identityToken else {
+                print("Unable to fetch identity token")
+                return
+            }
+            guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+                print("Unable to serialize token string from data: \(appleIDToken.debugDescription)")
+                return
+            }
+            // Initialize a Firebase credential.
+            let credential = OAuthProvider.credential(withProviderID: "apple.com",
+                                                      idToken: idTokenString,
+                                                      rawNonce: nonce)
+            // Sign in with Firebase.
+            Auth.auth().signIn(with: credential) { (authResult, error) in
+                guard let error = error else {
+                    // Error. If error.code == .MissingOrInvalidNonce, make sure
+                    // you're sending the SHA256-hashed nonce as a hex string with
+                    // your request to Apple.
+                    print(error)
+                    return
+                }
+                // User is signed in to Firebase with Apple.
+                // ...
+            }
+        }
+    }
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        // Handle error.
+        print("Sign in with Apple errored: \(error)")
     }
 }
 
